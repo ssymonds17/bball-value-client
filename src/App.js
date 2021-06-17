4b43d430aab5e632dc60dfca29bcefd5af8b1521
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-06-17 16:07:32 +0100

    Create views folder and file for each view on the site

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Player from './views/players/Player';
+import Team from './views/teams/Team';
+
 function App() {
   return (
     <div>
       <h1>App.js</h1>
+      <Player />
+      <Team />
     </div>
   );
 }
---
+++ src/views/players/Player.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Player() {
+  return (
+    <div>
+      <h1>Player Page</h1>
+    </div>
+  );
+}
