d9942cc63582fddd5a50ead48e0e59cd36bc6a70
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-06-17 16:45:09 +0100

    Add axios to project

--- src/views/players/PlayerIndex.js
+++ src/views/players/PlayerIndex.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function PlayerIndex() {
+  const [playerList, setPlayerList] = useState([]);
+
+  useEffect(() => {}, []);
+
   return (
     <div>
       <h1>Player Index</h1>
