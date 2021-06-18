9ba8752c65a963a14b2c5dc7875193d54b78a8fb
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-06-18 17:52:41 +0100

    Complete overall season view

--- src/apis/season.js
+++ src/apis/season.js
@@ -1,6 +1,21 @@
 import axios from 'axios';
 import { baseUrl } from './base';
 
+// Get list of all seasons including champions and MVPs
+export const fetchOverallSeason = async (league, year) => {
+  try {
+    const response = await axios.get(
+      `${baseUrl}/seasons/overall/${league}/${year}`
+    );
+    console.log(response.data);
+
+    return response.data;
+  } catch (err) {
+    throw err.response.data;
+  }
+};
+
+// Get list of all seasons including champions and MVPs
 export const fetchSeasonIndex = async () => {
   try {
     const response = await axios.get(`${baseUrl}/seasonindex`);
--- src/views/seasons/Overall.js
+++ src/views/seasons/Overall.js
@@ -1,9 +1,86 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchOverallSeason } from '../../apis/season';
+import { extractLeague, extractYear } from '../../helpers/season';
 
 export default function Overall() {
+  const [seasonData, setSeasonData] = useState(null);
+  const [thisLeague, setThisLeague] = useState(null);
+  const [thisYear, setThisYear] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  let rank = 0;
+
+  const loadOverallSeason = async (league, year) => {
+    const season = await fetchOverallSeason(league, year);
+    setSeasonData(season);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    const league = extractLeague();
+    const year = extractYear();
+    setThisLeague(league);
+    setThisYear(year);
+    loadOverallSeason(league, year);
+  }, []);
+
   return (
     <div>
-      <h1>Season Overall</h1>
+      {isLoading && (
+        <div>
+          <h2>Loading....</h2>
+        </div>
+      )}
+      {!isLoading && seasonData && (
+        <div>
+          <h1>
+            {thisLeague} {thisYear} Overall Season Statistics
+          </h1>
+          <table>
+            <thead>
+              <tr>
+                <th>Rank</th>
+                <th>Name</th>
+                <th>Value</th>
+                <th>Age</th>
+                <th>Pos</th>
+                <th>Score</th>
+                <th>G</th>
+                <th>MP</th>
+                <th>Val%</th>
+                <th>Score</th>
+                <th>G</th>
+                <th>MP</th>
+                <th>Val%</th>
+                <th>Tm Record</th>
+                <th>Tm Result</th>
+              </tr>
+            </thead>
+            <tbody>
+              {seasonData.map((player) => {
+                return (
+                  <tr key={player.player_id + player.rs_tm}>
+                    <td>{(rank += 1)}</td>
+                    <td>{player.player_name}</td>
+                    <td>{Number(player.season_value).toFixed(2)}</td>
+                    <td>{player.rs_age}</td>
+                    <td>{player.rs_pos}</td>
+                    <td>{Number(player.rs_score).toFixed(2)}</td>
+                    <td>{player.rs_g}</td>
+                    <td>{player.rs_mp}</td>
+                    <td>{player.rs_val_perc}</td>
+                    <td>{Number(player.po_score).toFixed(2)}</td>
+                    <td>{player.po_g}</td>
+                    <td>{player.po_mp}</td>
+                    <td>{player.po_val_perc}</td>
+                    <td>{player.tm_record}</td>
+                    <td>{player.tm_result}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 }
