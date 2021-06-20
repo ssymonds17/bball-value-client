02271fa024e4dafcdbd4ac313f8c5dbc38b3086f
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-06-20 16:26:11 +0100

    Add navigation links to season index view

--- src/views/seasons/SeasonIndex.js
+++ src/views/seasons/SeasonIndex.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { fetchSeasonIndex } from '../../apis/season';
 
 export default function SeasonIndex() {
@@ -39,18 +40,51 @@ export default function SeasonIndex() {
               {seasonsData.map((season) => {
                 return (
                   <tr key={season.year + season.league}>
-                    <td>{season.year}</td>
-                    <td>{season.league}</td>
-                    <td>{season.champions}</td>
-                    <td>{season.runners_up}</td>
                     <td>
-                      {season.overall_mvp} ({season.overall_mvp_score})
+                      <Link
+                        to={`/seasons/overall/${season.league}/${season.year}`}
+                      >
+                        {season.year}
+                      </Link>
                     </td>
                     <td>
-                      {season.rs_mvp} ({season.rs_mvp_score})
+                      <Link
+                        to={`/seasons/overall/${season.league}/${season.year}`}
+                      >
+                        {season.league}
+                      </Link>
                     </td>
                     <td>
-                      {season.playoffs_mvp} ({season.playoffs_mvp_score})
+                      <Link
+                        to={`teams/${season.champions_franchise_code}/${season.year}`}
+                      >
+                        {season.champions}
+                      </Link>
+                    </td>
+                    <td>
+                      <Link
+                        to={`teams/${season.runners_up_franchise_code}/${season.year}`}
+                      >
+                        {season.runners_up}
+                      </Link>
+                    </td>
+                    <td>
+                      <Link to={`/players/${season.overall_mvp_player_id}`}>
+                        {season.overall_mvp}
+                      </Link>{' '}
+                      ({season.overall_mvp_score})
+                    </td>
+                    <td>
+                      <Link to={`/players/${season.rs_mvp_player_id}`}>
+                        {season.rs_mvp}
+                      </Link>{' '}
+                      ({season.rs_mvp_score})
+                    </td>
+                    <td>
+                      <Link to={`/players/${season.playoffs_mvp_player_id}`}>
+                        {season.playoffs_mvp}
+                      </Link>{' '}
+                      ({season.playoffs_mvp_score})
                     </td>
                   </tr>
                 );
