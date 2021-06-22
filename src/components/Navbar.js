efc55dbefaa1d224d2d3fa63eab8d91e0f3d4585
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-06-22 18:47:25 +0100

    Set styling for navbar component on large screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/navbar.css';
 
 export default function Navbar() {
   return (
-    <div>
-      <Link to={`/`}>
-        <button>Home</button>
-      </Link>
-      <Link to={`/players`}>
-        <button>Players</button>
-      </Link>
-      <Link to={`/teams`}>
-        <button>Teams</button>
-      </Link>
-      <Link to={`/seasons`}>
-        <button>Seasons</button>
-      </Link>
-      <Link to={`/rankings/players`}>
-        <button>Greatest Players</button>
-      </Link>
-      <Link to={`/rankings/teams`}>
-        <button>Greatest Teams</button>
-      </Link>
-      <Link to={`/rankings/seasons/overall`}>
-        <button>Greatest Seasons</button>
-      </Link>
+    <div className='navbar-container'>
+      <div className='navbar'>
+        <Link to={`/`}>
+          <button className='nav-btn'>Home</button>
+        </Link>
+        <Link to={`/players`}>
+          <button className='nav-btn'>Players</button>
+        </Link>
+        <Link to={`/teams`}>
+          <button className='nav-btn'>Teams</button>
+        </Link>
+        <Link to={`/seasons`}>
+          <button className='nav-btn'>Seasons</button>
+        </Link>
+        <Link to={`/rankings/players`}>
+          <button className='nav-btn'>Greatest Players</button>
+        </Link>
+        <Link to={`/rankings/teams`}>
+          <button className='nav-btn'>Greatest Teams</button>
+        </Link>
+        <Link to={`/rankings/seasons/overall`}>
+          <button className='nav-btn'>Greatest Seasons</button>
+        </Link>
+      </div>
     </div>
   );
 }
