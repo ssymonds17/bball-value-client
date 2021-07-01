23537f8fc93e67f3389786e480d050b3116ce42e
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-07-01 10:45:18 +0100

    Add function to reset navbar so the drop down menu disappears once a route has been selected by the user

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,10 @@ import { Link } from 'react-router-dom';
 import '../styles/navbar.css';
 
 export default function Navbar() {
+  const resetNavbar = () => {
+    document.getElementById('menu-toggle').checked = false;
+  };
+
   return (
     <div className='navbar-container'>
       <div className='navbar'>
@@ -30,7 +34,7 @@ export default function Navbar() {
       </div>
       <nav>
         <Link to={`/`}>
-          <button className='home-brand'>NBA Value Index</button>
+          <button className='home-brand'>NBA Index</button>
         </Link>
         <input type='checkbox' id='menu-toggle' />
         <label htmlFor='menu-toggle' className='hamburger'>
@@ -38,22 +42,34 @@ export default function Navbar() {
         </label>
         <div className='nav-links'>
           <Link to={`/players`}>
-            <button className='nav-btn-drop'>Players</button>
+            <button className='nav-btn-drop' onClick={() => resetNavbar()}>
+              Players
+            </button>
           </Link>
           <Link to={`/teams`}>
-            <button className='nav-btn-drop'>Teams</button>
+            <button className='nav-btn-drop' onClick={() => resetNavbar()}>
+              Teams
+            </button>
           </Link>
           <Link to={`/seasons`}>
-            <button className='nav-btn-drop'>Seasons</button>
+            <button className='nav-btn-drop' onClick={() => resetNavbar()}>
+              Seasons
+            </button>
           </Link>
           <Link to={`/rankings/players`}>
-            <button className='nav-btn-drop'>Greatest Players</button>
+            <button className='nav-btn-drop' onClick={() => resetNavbar()}>
+              Greatest Players
+            </button>
           </Link>
           <Link to={`/rankings/teams`}>
-            <button className='nav-btn-drop'>Greatest Teams</button>
+            <button className='nav-btn-drop' onClick={() => resetNavbar()}>
+              Greatest Teams
+            </button>
           </Link>
           <Link to={`/rankings/seasons/overall`}>
-            <button className='nav-btn-drop'>Greatest Seasons</button>
+            <button className='nav-btn-drop' onClick={() => resetNavbar()}>
+              Greatest Seasons
+            </button>
           </Link>
         </div>
       </nav>
